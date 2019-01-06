bd93f3a579a1976a42f3e9a772cb07dec598012b
Cesar Gomez <zootechdrum@gmail.com>
2019-01-05 17:29:55 -0800

    added a better solution to naiveString

--- naiveString.js
+++ naiveString.js
@@ -18,3 +18,19 @@ function native(string1,string2){
     return counter
   }
   native("hellosdfsdfgshello","hello")
+
+  //alternate solution
+
+  function naiveSearch(long, short){
+      let count = 0;
+      for(let i = 0; i<long.length; i++){
+          for(var j = 0; j< short.length; j++){
+              if(short[j] !== long[i +j]){
+                  break
+              }
+              if(j === short.length - 1){
+                  count++
+              }
+          }
+      }
+  }
