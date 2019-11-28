2d161c1d766d5d8758add96c2b0fa43ab231ec40
Cesar Gomez <zootechdrum@gmail.com>
2019-11-28 10:43:39 -0800

    added cashRegister file problem from freeCodeCamp

---
+++ cashRegister.js
@@ -0,0 +1,17 @@
+
+// You have to create a program that will handle when the register does not have enough cash or will have no cash after the transaction. 
+// Other than that it needs to return an array of the change in the form of an array, so that will be a 2D array.
+
+// Hint: 1
+// It is easier to handle if you have to close the register, or if you know how much money is in your register beforehand and you will not have enough money to complete the transaction. For this it is recommended to have a function to assign this information to a variable.
+// try to solve the problem now
+
+// Hint: 2
+// Life is easier when you get to know the value of each currency type in the register instead of how much money is composed of that particular currency. So be sure to watch out for that.
+// try to solve the problem now
+
+//  Hint: 3
+// You will have to get as much change from one type before moving to the next from greater value to lesser, and keep going until you have covered the whole change.
+// try to solve the problem now
+
+// My monser of a solution below
