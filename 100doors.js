aacd4e89687ee127fcc73abaaec44ef76ca4770d
Cesar Gomez <zootechdrum@gmail.com>
2019-11-28 10:56:47 -0800

    added format to 100doors.js removed spacing between lines

--- 100doors.js
+++ 100doors.js
@@ -1,29 +1,17 @@
 function getFinalOpenedDoors(numDoors) {
     //Fills array1 with falsy value
-  
     let array1 = Array(100).fill(false);
-  
     let finalAnswer = []
-  
     let check = [];
     let ans;
   
     for (var i = 1; i <= numDoors; i++) {
       check.push(i);
     }
-  
-    console.log( check )
-  
     for (var i = 0; i <= check.length; i++) {
-      console.log(i)
-  
       for (var j = 1; j <= check.length; j++) {
-  
-  
         //Before we multiply we toggle the inital door
-  
         //Gets the multiples of the array
-  
          ans = check[i] * j;
          console.log("ans = " + ans)
   
@@ -31,10 +19,7 @@ function getFinalOpenedDoors(numDoors) {
                ans = undefined
                j = 101
           }
-  
-  
         //checks if the multiple obove exists in the array of check
-  
         let marker = check.indexOf(ans);
         console.log('marker' + marker)
   
@@ -42,18 +27,14 @@ function getFinalOpenedDoors(numDoors) {
             console.log(marker)
           array1[marker] = !array1[marker];
         }
-            console.log(array1)
       }
-  
     }
-  
     for ( var l = 0; l <= array1.length; l++){
         if (array1[l] === true){
             finalAnswer.push(check[l])
         }
         console.log("final Answer " + finalAnswer)
     }
-  
     return finalAnswer;
   }
   
