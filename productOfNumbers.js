ff7686a49a6d74457ebf46ba8f5f5bb7ea9df648
Cesar Gomez <zootechdrum@gmail.com>
2019-12-26 18:05:06 -0800

    add comments to productOfnumbers file

--- productOfNumbers.js
+++ productOfNumbers.js
@@ -7,3 +7,26 @@
 
 
 // Assume the argument array contains only positive integers
+
+function otherProd(arr){
+    let ans = []
+    let red = []
+   
+     for( let i = 0; i < arr.length; i++){
+       for(let j = 0; j < arr.length; j++){
+         //only i and j are not the same will the value
+         //be pushed to the red array.
+         if ( j !== i) {
+             red.push(arr[j])
+         }
+       }
+       //Reduces the value of the array
+       let resultOfMult = red.reduce( (a,b) => (a * b ));
+       //pushed the resulting value to our ans Array.
+       ans.push(resultOfMult)
+       red = []
+    
+     }
+   }
+   
+   otherProd([10,2,5,4])
