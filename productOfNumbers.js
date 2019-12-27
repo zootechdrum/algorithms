aa523df101b82ca35e79b4779fa4bd5bf03d0cac
Cesar Gomez <zootechdrum@gmail.com>
2019-12-26 18:02:03 -0800

    add solution and file to productOfNumber challenge

---
+++ productOfNumbers.js
@@ -0,0 +1,9 @@
+//Product of all other numbers
+
+// Write a function otherProduct that takes in an array of numbers and returns a new array where each element within the return array is the product of multiplying all the numbers in the argument array except the element corresponding to the current index.
+
+
+// Assume the argument array has at least 2 elements
+
+
+// Assume the argument array contains only positive integers
