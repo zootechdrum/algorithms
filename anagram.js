7b34f010aed96d18093b618b990a102d814c5224
Cesar Gomez <zootechdrum@gmail.com>
2020-02-27 21:42:52 -0800

    added potential solution

--- anagram.js
+++ anagram.js
@@ -23,3 +23,42 @@
 //If it is great if its not return false
 
 //We will then check if the value of key in object 2 has the same value of the current key in object 1
+
+function anagram(str1, str2){
+    //convert each string into an array
+    let arr1 = str1.split("");
+    let arr2 = str2.split("");
+
+    //If arrays do not equal each other we can return right away
+
+    if(arr1.length !== arr2.length) {
+        return false;
+    }
+
+    let freqCount1 = {};
+    let freqCount2 = {};
+
+    for(let val of arr1){
+        //adds key to freqCount1 and sets is equal to zero or adds 1 if the value does exist
+        //If the value does not exist yet then it will show undefined
+        //But because we have the or operator we set it to zero to have a value
+
+        freqCount1[val] = (freqCount1[val] || 0) + 1;s
+    }
+
+    for(let val of arr2){
+        freqCount2[val] = (freqCount2[val] || 0) + 1;
+    }
+
+    for(let key in freqCount1){
+        if(!(key in freqCount2)){
+            return false
+        }
+        if(freqCount2[key] !== freqCount1[key] ){
+            return false
+        }
+    }
+    return true;
+}
+
+anagram("anagram","nagaram")
