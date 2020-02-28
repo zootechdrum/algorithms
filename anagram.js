416b66708d6513605b4415c5ce7f0ce835b2371b
Cesar Gomez <zootechdrum@gmail.com>
2020-02-27 21:17:17 -0800

    added anagram challenge

---
+++ anagram.js
@@ -0,0 +1,7 @@
+//Given two strings, write a function to determine ifthe second string is an anagram of the first.
+
+//Ex validAnagram("","") true
+
+//Ex validAnagram("aaz","zza") false
+
+//Ex validAnagram("anagram","nagaram") false
