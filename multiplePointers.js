f4819f5c0aecc960411110a57aca6610cb366f23
Cesar Gomez <zootechdrum@gmail.com>
2020-02-28 20:00:19 -0800

    defined loop also initialized variable i outside of loop

---
+++ multiplePointers.js
@@ -0,0 +1,32 @@
+// Instructions
+
+// A grerat problem for the multiple pointers strategy
+
+// Implement a funciton called countUniqueValues, which accepts
+// a sorted Array, and counts the unique values in the Array. There can 
+// be negative numbers in the Array, but it will always be sorted. 
+
+function countUniqueValues() {
+
+//We want to have 2 pointers.
+// One pointer will keep track of the current number in the array
+// Second pointer will slowly move up and compare values agains our first pointer
+
+
+
+//I needs to be initialized outside of loop
+//Else it would keep being eqaul to 0.
+let i = 0;
+//This is the loop that will hold 2 pointers
+for(var i , j = 0; j< arr.lenght; j++){
+
+
+}
+
+}
+
+countUniqueValues([1,1,1,1,1,2]) // 2
+
+countUniqueValues([]) // 0
+
+countUniqueValues([-2,-1,-1,0,1]) // 2
