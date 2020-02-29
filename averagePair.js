e299b351aabb9f903ca103e7645f682e03c79e7f
Cesar Gomez <zootechdrum@gmail.com>
2020-02-29 13:32:13 -0800

    solved isSubsequence algo with multiple pointers tchnique

--- averagePair.js
+++
@@ -1,8 +0,0 @@
-// Multiple Pointers - isSubsequence
-
-// Write a function called isSubsquence which takes in two strings and checks whter the charecters in the first string
-// form a subsequence of the charecters in the second string. In other WEBGL_compressed_texture_s3tc, the function should check
-// whter the charecters in the first string appear somewhere in the second string, withour their order changing. 
-
-isSubsequence("hello", "hello world") // true
-isSubsequence("sing", "sting") // true
