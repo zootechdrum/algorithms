bcb414063be27d3bea9be2c56936cd9d9bd556b6
Cesar Gomez <zootechdrum@gmail.com>
2020-02-29 10:31:13 -0800

    added new file algo to check if there are dups

---
+++ areThereDuplicates.js
@@ -0,0 +1,7 @@
+//Implement a function called areThereDuplicated which accepts a variable number of
+//of arguments, and checks whter there are any duplicates among the arguments passed in.
+// You can solve this using the frerquency counter patter or the multiple pointrs pattern
+
+function areThereDuplicates() {
+    // good luck. (supply any arguments you deem necessary.)
+  }
