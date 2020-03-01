a2b6a1b1adf09594c5249d363c223ad0fe4ea5fe
Cesar Gomez <zootechdrum@gmail.com>
2020-03-01 12:48:36 -0800

    changed string to an array

--- freqCounter&2pointers/findLongestSubstring.js
+++ freqCounter&2pointers/findLongestSubstring.js
@@ -1,4 +1,11 @@
 // Write a function called findLongestSubstring which accepts a string and returns the length
 // of the longest substring with all distinct charechters.
+
+function findLongestSubstring(str){
+
+    let arr = str.split('');
+
+}
+
 findLongestSubstring('') // 0
 findLongestSubstring('rithmschol') // 7
