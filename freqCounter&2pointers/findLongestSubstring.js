62f410ef19ffffbe9591a0627ff84081abc991c3
Cesar Gomez <zootechdrum@gmail.com>
2020-03-01 12:47:28 -0800

    added new algo file that you used sliding window pattern on

---
+++ freqCounter&2pointers/findLongestSubstring.js
@@ -0,0 +1,4 @@
+// Write a function called findLongestSubstring which accepts a string and returns the length
+// of the longest substring with all distinct charechters.
+findLongestSubstring('') // 0
+findLongestSubstring('rithmschol') // 7
