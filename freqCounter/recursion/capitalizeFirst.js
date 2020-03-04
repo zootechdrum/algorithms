259a978196dad5a5b17667fcd2cb60bcee827c7a
Cesar Gomez <zootechdrum@gmail.com>
2020-03-03 17:47:07 -0800

    Finished recusive function to capitilize every letter in array

--- freqCounter/recursion/capitalizeFirst.js
+++ freqCounter/recursion/capitalizeFirst.js
@@ -1,5 +1,19 @@
 function capitalizeFirst(arr){
 
+    let ansArr = [];
+
+    if(arr.length === 0){
+        return ansArr;
+    }
+
+    if( arr.length > 0){
+        let capitilize = arr[0].charAt(0).toUpperCase() + arr[0].slice(1)
+        ansArr.push(capitilize);
+    }
+
+    ansArr = ansArr.concat(capitalizeFirst(arr.slice(1)))
+    console.log(ansArr)
+    return ansArr;
 }
 
 capitalizeFirst(['car','taco','banana'])
