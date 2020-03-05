7a793ea10ee103e4366dcdf738d5faf783f9878a
Cesar Gomez <zootechdrum@gmail.com>
2020-03-04 18:04:17 -0800

    added testing file

---
+++ freqCounter/recursion/flattArray.js
@@ -0,0 +1,11 @@
+function flatten(arr){
+
+    let newArr = [];
+
+    if (arr.length === 0) {
+        return newArr;
+    } 
+
+}
+
+ flatten([1, 2, 3, [4, 5] ]) // [1, 2, 3, 4, 5]
---
+++ freqCounter/recursion/test.js
@@ -0,0 +1,5 @@
+function testing(){
+    let arr = [[[1]]]
+    console.log(arr[0])
+
+}
