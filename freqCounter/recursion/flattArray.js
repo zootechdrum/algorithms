dd971a4d9e2bf13963088a586fb8f8830a9bc1aa
Cesar Gomez <zootechdrum@gmail.com>
2020-03-05 21:48:51 -0800

    added comments to recursive solution

--- freqCounter/recursion/flattArray.js
+++ freqCounter/recursion/flattArray.js
@@ -1,16 +1,26 @@
 function flatten(arr){
+    //Init new Arr
     let newArr = [];
+    //Console.log to show where we are at after
+    //each iterative call
     console.log(arr)
+
+    //If arr has the length of 0 reuturn newArr. This serves as a base case
     if (arr.length === 0) {
-        return newArr;
+        return newArr
     } 
+
+    //If our current index is of type Array
     if(Array.isArray(arr[0])){
-        let conc = newArr.concat(flatten(arr[0]))
-        return newArr.concat(conc)
+        //set newArr to whatever concatonated array gets returned 
+         newArr = newArr.concat(flatten(arr[0]))
     } else{
+        //Else just push the number to newArr
         newArr.push(arr[0])
     }
+    //set newArr to newArr and concat whatever gets returned after slicing one from array
     newArr = newArr.concat(flatten(arr.splice(1)))
+    //eventualy return newArr
     return newArr;
 }
-  flatten([1, [2, [3, 4], [[5]]]]) // [1, 2, 3, 4, 5]
+  flatten([1, [2, [3, 4], [[5]]]]) // [1, 2, 3, 4, 5].
