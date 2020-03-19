e79deff2027668409a7d1139bf273b822db15bd7
Cesar Gomez <zootechdrum@gmail.com>
2020-03-19 10:59:50 -0700

    added python

---
+++ post-bootcamp/algo-array-sorted/algo-array-sorted.py
@@ -0,0 +1,44 @@
+
+#  We are creating a function that takes in an unsorted array of integers
+#  and returns a sorted array with all the even numbers first in ascending order
+#  and all the odd numbers in descending order after the even numbers
+
+#  -------------------------------------------------------
+#                 Helper Functions (Optional)
+
+#  -------------------- Your Code Here --------------------
+
+
+
+
+
+#  --------------------- End Code Area --------------------
+
+
+#  -------------------------------------------------------
+                        # Sort Function
+#  -------------------------------------------------------
+def up_down_start(arr):
+
+    evenlist = []
+
+#    -------------------- Your Code Here --------------------
+    for x in arr:
+        if x % 2 == 0:
+            if len(thelist) == 0:
+
+
+
+
+
+#    --------------------- End Code Area --------------------
+
+
+
+#  ------------------------------------------------------------------
+
+testArr1 = [5, 32, 9, 47, 22, 6, 33, 17, 20, 73]
+# console.log("The following should be [6, 20, 22, 32, 73, 47, 33, 17, 9, 5]");
+up_down_start(testArr1)
+
+# // ------------------------------------------------------------------
