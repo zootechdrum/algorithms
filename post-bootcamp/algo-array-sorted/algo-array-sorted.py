d2b4077f478fe45aea967f2618cccd2367e4901f
Cesar Gomez <zootechdrum@gmail.com>
2020-03-19 16:48:46 -0700

    added solution to python file

--- post-bootcamp/algo-array-sorted/algo-array-sorted.py
+++ post-bootcamp/algo-array-sorted/algo-array-sorted.py
@@ -1,53 +1,36 @@
-
-#  We are creating a function that takes in an unsorted array of integers
-#  and returns a sorted array with all the even numbers first in ascending order
-#  and all the odd numbers in descending order after the even numbers
-
-#  -------------------------------------------------------
-#                 Helper Functions (Optional)
-
-#  -------------------- Your Code Here --------------------
-def sortEven(arr):
+def sort(arr , cond):
     for i in range(0, len(arr)):
         for j in range(i + 1, len(arr)):
-            if(arr[i] > arr[j]):
-                temp = arr[i]
-                arr[i] = arr[j]
-                arr[j] = temp
+            if( cond == 'even'):
+                if(arr[i] > arr[j]):
+                    temp = arr[i]
+                    arr[i] = arr[j]
+                    arr[j] = temp
+            else:
+                if(arr[i] > arr[j]):
+                    temp = arr[i]
+                    arr[i] = arr[j]
+                    arr[j] = temp
+
     return arr
-
-
-
-#  --------------------- End Code Area --------------------
-
-
-#  -------------------------------------------------------
-                        # Sort Function
-#  -------------------------------------------------------
+#The below function takes an array and checks if numbers are devided by 2
 def up_down_start(arr):
 
     evenlist = []
     oddlist = []
 
-#    -------------------- Your Code Here --------------------
+#   Depending if number is even or odd the numbers will be added to even or odd list
     for x in arr:
         if x % 2 == 0:
             evenlist.append(x)
         else:
              oddlist.append(x)
-    
-    assortedEven = sortEven(evenlist)
-    print(assortedEven)
-
-
-
-
-
-#    --------------------- End Code Area --------------------
-
-
-
-#  ------------------------------------------------------------------
+  #Helper function gets called with appropriate second para  
+    assortedEven = sort(evenlist, 'even')
+    assortedOdd = sort(oddlist,"odd")
+    joined_list = assortedEven + assortedOdd
+    #Concat the two lists. 
+    return joined_list
 
 testArr1 = [5, 32, 9, 47, 22, 6, 33, 17, 20, 73]
 # console.log("The following should be [6, 20, 22, 32, 73, 47, 33, 17, 9, 5]");
