7fdc7b4d2f308c9a48d7942effba300048684f77
Cesar Gomez <zootechdrum@gmail.com>
2020-03-21 19:54:48 -0700

    added helper function

---
+++ interviewCake/reverseWord/reverseWord.js
@@ -0,0 +1,18 @@
+
+const reverseWords = (word) => {
+    
+    reverseOrder(0, word, word.length)
+
+}
+
+const reverseOrder = (start, word, end) => {
+    console.log(word)
+
+}
+
+
+message = [ 'c', 'a', 'k', 'e', ' ',
+'p', 'o', 'u', 'n', 'd', ' ',
+'s', 't', 'e', 'a', 'l' ]
+
+reverseWords(message)
