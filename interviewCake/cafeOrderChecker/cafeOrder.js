92ca15c1f487b7395c6c346af8b684e5c2a96eb9
Cesar Gomez <zootechdrum@gmail.com>
2020-03-22 18:10:39 -0700

    added working solution to cafeOrder

--- interviewCake/cafeOrderChecker/cafeOrder.js
+++ interviewCake/cafeOrderChecker/cafeOrder.js
@@ -1,31 +1,32 @@
-const resterOrders = (takeOut, dineIn, serverOrders) => {
+const resterOrders = (takeOut, dineIn, servedOrders) => {
 
     let takeOutPointer = 0;
     let dineInPointer = 0;
     let firstComeFirstServe = true;
     
 
-    for(let i = 0 ; i < serverOrders.length; i++){
-        if( serverOrders[i] !== dineIn[dineInPointer] || servedOrders[i] !== takeOut[takeOutPointer] ){
-            console.log(dineIn[dineInPointer])
-            firstComeFirstServe = false;
+    for(let i = 0 ; i < servedOrders.length; i++){
+
+        if(servedOrders[i] !== takeOut[takeOutPointer] && servedOrders[i] !== dineIn[dineInPointer] ){
+            firstComeFirstServe = false
+        } else if( servedOrders[i] === takeOut[takeOutPointer]){
+            ++takeOutPointer
+        } else if(servedOrders[i] === dineIn[dineInPointer]){
+            ++dineInPointer
         }
-        
-        dineInPointer += dineInPointer
-        takeOutPointer += takeOutPointer
     }
 
     console.log(`Is the current state of resteraunt first come first serve: ${firstComeFirstServe}`)
 
 }
 
+// const takeOutOrders = [1, 3, 5]
+// const dineInOrders = [2, 4, 6]
+// const servedOrders = [1, 2, 3, 5, 4, 6]
+
 const takeOutOrders = [1, 3, 5]
 const dineInOrders = [2, 4, 6]
-const servedOrders = [1, 2, 3, 5, 4, 6]
-
-// const TakeOutOrders = [1, 3, 5]
-// const dineInOrders = [2, 4, 6]
-// const servedOrders = [1, 2, 4, 6, 5, 3]
+const servedOrders = [1, 2, 4, 6, 5, 3]
 
 resterOrders(takeOutOrders, dineInOrders, servedOrders)
 
