9767264e6f0292d193a612e707a0c9bf65d3d9f8
Cesar Gomez <zootechdrum@gmail.com>
2020-03-24 12:05:46 -0700

    successfuly added solution to permutations

--- interviewCake/permutations/perms.js
+++ interviewCake/permutations/perms.js
@@ -2,19 +2,36 @@
 const palindrome = (word) => {
 
     const ansObj = {}
+    let odd = 0;
 
     for(let i = 0; i < word.length; i ++){
 
         ansObj[word[i]] = ansObj[word[i]] + 1 || 0 + 1
     }
     
+    console.log(ansObj)
     //loop through the object to make sure there is only one 
     //with the key values as one
+
+    for(const property in ansObj) {
+        console.log(ansObj[property])
+        if ( ansObj[property] === 1){
+            ++odd
+        }
+        console.log(odd)
+
+        if(odd === 2){
+            console.log("not a palindrome")
+            return 'not a palindrome'
+        }
+
+    }
+    return "is a palindrome"
 }
 
 
 
-palindrome('ivicc')
+palindrome('civil')
 
 
 
