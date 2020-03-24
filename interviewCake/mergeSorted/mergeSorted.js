d88bb5107bf61cee83ed374728f68c38dc0abc4d
Cesar Gomez <zootechdrum@gmail.com>
2020-03-24 09:37:01 -0700

    added perms folder

--- interviewCake/mergeSorted/mergeSorted.js
+++ interviewCake/mergeSorted/mergeSorted.js
@@ -1,17 +1,11 @@
-//We have our lists of orders sorted numerically already, 
-//in arrays. Write a function to merge our arrays of orders into one sorted array.
-
 const myArray = [3, 4, 6, 10, 11, 15];
 const alicesArray = [1, 5, 8, 12, 14, 19];
 let comparedPointer = 0;
 let longestPointer = 0
 let longestArr;
 let comparedArr;
-
 const ansArr = [];
-
-
-//We have to determine the longest array to make sure we
+//We have to de_termine the longest array to make sure we
 //iterate over all numbers
 if (myArray.length >= alicesArray.length) {
     longestArr = myArray
@@ -23,9 +17,7 @@ if (myArray.length >= alicesArray.length) {
 //The variable below adds up the total length of both of our arrays
 let totalArrayMerges = longestArr.length + comparedArr.length
 
-
 const mergeArrays = (arr1, arr2) => {
-
     //While our ans array length is less than the combined total of both of our arrays
     //loop
     while (ansArr.length < totalArrayMerges) {
@@ -49,10 +41,9 @@ const mergeArrays = (arr1, arr2) => {
             } else {
                 ansArr.push(longestArr[longestPointer])
             }
-
         }
     }
+    console.log(ansArr)
     return ansArr;
 }
-
 mergeArrays(myArray, alicesArray)
