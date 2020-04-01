3ed85e242e4212d2952a979caf4646fa5254027a
Cesar Gomez <zootechdrum@gmail.com>
2020-03-31 17:47:37 -0700

    added new challenge and added new comments for challenge

---
+++ interviewCake/highScore/getProductsOfInt/getProduct.js
@@ -0,0 +1,10 @@
+//You have an array of integeers , and for each index you want to find the product of every
+//integer except the integer at the index. 
+
+
+const highestProduct = (intArr) => {
+    console.log(intArr)
+
+}
+
+highestProduct([3,1,2,5,6,4])
--- interviewCake/highScore/highScore.js
+++ interviewCake/highScore/highScore.js
@@ -1,20 +1,24 @@
 function sortScores(unsortedScores, HIGHEST_POSSIBLE_SCORE){
 
-    const arr = Array(HIGHEST_POSSIBLE_SCORE + 1).fill(0)
 
+    //Create an array of 100 items zero is included
+    const arr = Array(HIGHEST_POSSIBLE_SCORE + 1).fill(0)
+    //An array that will eventually hold our scored in order
     const orderHighScore = [];
-    
+    //Loops through unsorted array and increased counter in that index in our array arr
     for(let i = unsortedScores.length - 1; i > 0; i--){
         arr[unsortedScores[i]]++;
     }
-    //Lets say we have the same score twice. How do we proceed ?
+
+    //Iterates over entir arr aray starting from last element
+    //We start backwards beacuse all our highest scores will be at the end. 
     for(let j = arr.length; j > 0 ; j--){
+    //Nested loop that will only run if that slot in our array
+    //has a vaue greater than 0
         for(let k = arr[j]; k > 0;k-- ){
             orderHighScore.push(j)
         }
     }
-
-
 }
 
 const unsortedScores = [37, 89, 41, 65, 91, 53];
