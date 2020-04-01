20f3b3ed61afdfcb0ba3a0dd15b0e63cd588bc9c
Cesar Gomez <zootechdrum@gmail.com>
2020-03-31 17:54:00 -0700

    added new comments to highest product function

---
+++ interviewCake/getProductsOfInt/getProduct.js
@@ -0,0 +1,11 @@
+//You have an array of integeers , and for each index you want to find the product of every
+//integer except the integer at the index. 
+
+
+const highestProduct = (intArr , index) => {
+    //Keeps track of numbers before index.
+    const allNumbersBefore = [];
+
+}
+
+highestProduct([3,1,2,5,6,4])
