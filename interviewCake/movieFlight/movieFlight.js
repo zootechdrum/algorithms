c9981248b7141a6555043f4d9d5fd7dfc7d9622b
Cesar Gomez <zootechdrum@gmail.com>
2020-04-02 21:39:21 -0700

    used set in javascript

--- interviewCake/movieFlight/movieFlight.js
+++ interviewCake/movieFlight/movieFlight.js
@@ -1,15 +1,23 @@
 function movieFlight(durationOfFlight, movies) {
 
-    const movieLengths = new Set([...movies])
+    const movieLengths = new Set()
     
     for(let i = 0; i < movies.length; i++){
 
-        firstMovie = durationOfFlight - movie[i];
+        //We chose our first movie
+        let firstMovie = movies[i];
 
+        let secondMovie = durationOfFlight - movies[i]; 
+
+        if(movieLengths.has(secondMovie)){
+            return true
+        }
+
+        movieLengths.add(firstMovie)
         //How do we to the second movie
-
-        
     }
+
+    return false;
 
 }
 
