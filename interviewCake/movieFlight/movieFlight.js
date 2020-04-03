c020e731c3bdfe2522307d592df5a3cb4f5f3701
Cesar Gomez <zootechdrum@gmail.com>
2020-04-02 21:23:19 -0700

    added comments to file created a for loop to iterate over movie lengths

--- interviewCake/movieFlight/movieFlight.js
+++ interviewCake/movieFlight/movieFlight.js
@@ -1,5 +1,16 @@
 function movieFlight(durationOfFlight, movies) {
 
+    const movieLengths = new Set([...movies])
+    
+    for(let i = 0; i < movies.length; i++){
+
+        firstMovie = durationOfFlight - movie[i];
+
+        //How do we to the second movie
+
+        
+    }
+
 }
 
 movieFlight(120, [60, 30, 40, 90, 60])
